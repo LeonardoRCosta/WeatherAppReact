51fbe1e78c15e177387d52ad304046e20a7bca79
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-14 20:06:48 -0300

    Criado componente WeatherApp.jsx e comps importados.

---
+++ weatherapp/src/components/weatherApp/WeatherApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchBar from '../searchBar/SearchBar'
+import WeatherCard from '../weatherCard/WeatherCard'
+
+export default function WeatherApp() {
+  return (
+    <div>
+        <SearchBar />
+        <WeatherCard />
+    </div>
+
+  )
+}
