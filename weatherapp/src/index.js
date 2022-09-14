9fcbed9c5a1f420ef87049a9283d12d6213ec3ba
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-14 20:05:43 -0300

    Importado componente WeatherApp e index.css

--- weatherapp/src/index.js
+++ weatherapp/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import WeatherApp from './components/weatherApp/WeatherApp';
+import './index.css';
+ReactDOM.render(
+    <WeatherApp />, 
+    document.getElementById('root')
+);
