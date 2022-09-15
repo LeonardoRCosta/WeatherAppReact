6f7a88c9bf40040bb2474005c192f4f9ccd07810
LeonardoRCosta <rm94286@fiap.com.br>
2022-09-15 17:11:35 -0300

    Estilização principal do app finalizada.

--- weatherapp/src/components/weatherCard/WeatherCard.jsx
+++ weatherapp/src/components/weatherCard/WeatherCard.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { WeatherContainer } from '../../style/styled';
-import { Card } from '../../style/styled';
+import { WeatherContainer, Card, CityDiv, CityName,TempertureDiv, TemperatureText, FeelsLikeDiv, FeelsLikeText, WeatherDiv, WeatherText, DateDiv, DateText } from '../../style/styled';
 export default function WeatherCard() {
   return (
     <WeatherContainer className="container">
       <Card className="card">
-        <div className="city">
-          <h1>São Paulo</h1>
-        </div>
-        <div className="date">
+        <CityDiv className="city">
+          <i class="fa-solid fa-location-dot"></i>
+          <CityName>São Paulo</CityName>
+        </CityDiv>
+        <TempertureDiv className="temperature">
+          <p>Icone</p>
+          <TemperatureText>25 °C</TemperatureText>
+        </TempertureDiv>
+        <FeelsLikeDiv className="feelsLike">
+          <FeelsLikeText>Sensação 23 °C</FeelsLikeText>
+        </FeelsLikeDiv>
+        <WeatherDiv className="weather">
+          <WeatherText>Ensolarado</WeatherText>
+        </WeatherDiv>
+        <DateDiv className="date">
           {/* <h3>{dateBuilder(new Date())}</h3> */}
-          <h3>Quarta feira, 14 de Setembro</h3>
-        </div>
-        <div className="temperature">
-          <h1>25 °C</h1>
-        </div>
-        <div className="weather">
-          <h3>Ensolarado</h3>
-        </div>
+          <DateText>Quarta feira, 14 de Setembro</DateText>
+        </DateDiv>
       </Card>
     </WeatherContainer>
   );
--- weatherapp/src/style/styled.js
+++ weatherapp/src/style/styled.js
@@ -1,47 +1,99 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 export const SearchDiv = styled.div`
-    display: flex;
-    justify-content: center;
-    margin: 2vh 0 5vh 0;
+  display: flex;
+  justify-content: center;
+  margin: 2vh 0 5vh 0;
 `;
 
 export const Search = styled.input`
-    min-width: 18vw;
-    height: 3vh;
-    border: 1px solid rgb(255, 255, 255, 0.25);
-    border-radius: 4px;
-    transition: all 0.5s ease-out;
-    font-size: 1.2em;
-    background-color: rgb(255, 255, 255, 0.25);
-    &:focus {
-        outline: none;
-        height: 4vh;
-        background-color: #ebebe0;
-    }
-    ::placeholder {
-        font-size: 0.7em;
-        color: #000000;
-    }
+  width: 18vw;
+  min-width: 18vw;
+  height: 4vh;
+  min-height: 4vh;
+  border: 1px solid rgb(255, 255, 255, 0.25);
+  border-radius: 4px;
+  transition: all 0.5s ease-out;
+  font-size: 1.2em;
+  background-color: rgb(255, 255, 255, 0.25);
+  &:focus {
+    outline: none;
+    background-color: #ebebe0;
+  }
+  ::placeholder {
+    font-size: 0.7em;
+    color: #000000;
+  }
 `;
 
 export const SearchIcon = styled.i`
-    font-size: 1.2em;
-    margin: 0 0.5vw 0 0;
+  font-size: 1.2em;
+  margin: 0.3vh 0.5vw 0 -1vw;
 `;
 
 export const WeatherContainer = styled.div`
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 `;
 
 export const Card = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    min-height: 80vh;
-    min-width: 20vw;
-    padding: 1vw;
-    border-radius: 10px;
-    box-shadow: 5px 5px 10px #333333;
-    background-color: rgb(255, 255, 255, 0.25);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 80vh;
+  min-height: 80vh;
+  width: 20vw;
+  min-width: 250px;
+  padding: 1vw;
+  border-radius: 10px;
+  box-shadow: 5px 5px 10px #333333;
+  background-color: rgb(255, 255, 255, 0.25);
+`;
+
+export const CityDiv = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 0.5vw;
+  margin: 10vh 0 8vh 0;
+`;
+
+
+export const CityName = styled.h1`
+  font-size: 1.8em;
+  font-weight: 600;
+`;
+
+export const TempertureDiv = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5vh;
+`;
+
+export const TemperatureText = styled.h1`
+  font-size: 4em;
+  font-weight: 200;
+`;
+
+export const FeelsLikeDiv = styled.div`
+  margin-bottom: 5vh;
+`;
+
+export const FeelsLikeText = styled.p`
+  font-size: 1.2em;
+`;
+
+export const WeatherDiv = styled.div`
+  margin-top: 3vh;
+`;
+
+export const WeatherText = styled.p`
+  font-weight: 600;
+  font-size: 1.1em;
+`;
+
+export const DateDiv = styled.div`
+  margin-top: 8vh;
+`;
+
+export const DateText = styled.p`
+  font-size: 1.1em;
 `;
